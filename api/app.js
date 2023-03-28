e200495d696753a9932235552088cb1ad1fd0218
CodyAshby222 <cashby220@gmail.com>
2023-03-28 13:57:02 -0600

    added form response

--- api/app.js
+++ api/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const path = require("path");
 
+const pathToIndex = path.resolve(__dirname, "../client/index.html");
+
+app.use("/*", (request, response) => {
+  response.sendFile(pathToIndex);
+});
+
 const app = express();
 
 module.exports = app;
